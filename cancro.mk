e80271cbf1316973ed5b0cac59e1218cb6ae8b6d
jrizzoli <joey@cyanogenmoditalia.it>
2015-11-12 20:46:59 +0100

    cancro: update storage configs for M
    Signed-off-by: jrizzoli <joey@cyanogenmoditalia.it>

--- cancro.mk
+++ cancro.mk
@@ -7,14 +7,6 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
 PRODUCT_CHARACTERISTICS := nosdcard
 
-# USB
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    persist.sys.usb.config=mtp
-
-# set USB OTG enabled to add support for USB storage type
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.sys.isUsbOtgEnabled=1
-
 # Charger
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/root/chargeonlymode:root/sbin/chargeonlymode
